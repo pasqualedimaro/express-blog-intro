8e2eded1058ab9eb9bb02236d104a90bd8bd5069
pasqualedimaro <pakydm15@gmail.com>
2025-06-27 16:04:10 +0200

    start server on local pc

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 //mi creo il file index.js
 // installo express
 // creo un file gitignore che mi ignore il node_modules
+// mi scrivo gli script 
+// inizializzo express
+
+const express = require("express")
+const app = express()
+const port = 3050
+
+// avvio server di express nella porta che in questo caso è il mio pc
+
+app.listen(port,() =>{
+    console.log (`server avviato su http://localhost:${port}`)
+})
