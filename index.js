bd514fec6bbfac496c01b87bb5d0b752b23a5c7d
pasqualedimaro <pakydm15@gmail.com>
2025-06-27 16:40:20 +0200

    add arry to second route and add middle argoument

--- index.js
+++ index.js
@@ -8,6 +8,9 @@
 const express = require("express")
 const app = express()
 const port = 3050
+ // aggiungo argomento middle invocando la mia funzione con i post
+ app.use(express.static("public"))
+
 
 // avvio server di express nella porta che in questo caso è il mio pc
 
@@ -23,3 +26,42 @@ app.get("/", (req, res)=>(
 
 // creo una seconda rotta che mi deve restituire un oggetto json con la lista dei post
 
+app.get ("/api/posts", (req,res)=>{
+
+// aggiungo la costante e l'arrey direttamente nel get
+
+    const post = [
+    {
+        title:"Ciambellone",
+        image: "public/images/ciambellone.jpeg",
+        content: "dolce semplice da fare, perfetto per colazione",
+        tags: ["dolci", "ricetta","colazione"]
+    }, {
+        title:"Creakers alla barbabietola",
+        image: "public/images/cracker_barbabietola.jpeg",
+        content: "snack genuino per fare merenda ",
+        tags: ["snack", "barbabietola","merenda"]
+    }, {
+        title:"Pane fritto dolce",
+        image: "public/images/pane_fritto_dolce.jpeg",
+        content: "dolce tradizionale fritto e molto gustoso",
+        tags: ["dolce", "tradizione","fritto"]
+
+    }, {
+        title:"Pasta alla Barbabietola",
+        image: "public/images/pasta_barbabietola.jpeg",
+        content: "Primo piatto molto colorato e genuino",
+        tags: ["pasta", "barbabietola","primo"]
+
+    }, {
+        title:"Torta paesana",
+        image: "public/images/torta_paesana.jpeg",
+        content: "dolce rustico come lo facevano le nostre nonne ",
+        tags: ["torta", "nonna","uvetta"]
+
+    }
+]
+  // restituisco la lista con oggetto json
+  res.json(post)
+
+})
